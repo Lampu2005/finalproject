dc77445d4d6f49be1e9d3b50f96f68ccf5d31bb2
TL <61598435+Lampu2005@users.noreply.github.com>
2025-10-07 16:02:01 +0200

    Delete multi-cloud-k8s/moduli/aks directory

--- multi-cloud-k8s/moduli/aks/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "region" { type = string }
-variable "azure_subscription_id" { type = string }
-variable "azure_tenant_id"       { type = string }
-variable "azure_client_id"       { type = string }
-variable "azure_client_secret"   { type = string }
